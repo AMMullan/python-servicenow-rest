23b4a385dbb9996e2566aecea4c71a1dc9dffa91
Robert Wikman <rbw@vault13.org>
2015-04-29 10:09:32 +0200

    - Added PyPI setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name='servicenow_rest',
+    packages=['servicenow_rest'],
+    version='0.1',
+    description='ServiceNow REST API Client',
+    install_requires=['requests'],
+    author='Robert Wikman',
+    author_email='rbw@vault13.org',
+    maintainer='Robert Wikman',
+    maintainer_email='rbw@vault13.org',
+    url='https://github.com/rbw0/python-servicenow-rest',
+    download_url='https://github.com/rbw0/python-servicenow-rest/tarball/0.1',
+    keywords=['servicenow', 'rest', 'api'],
+    classifiers=[],
+    license='GPLv2',
+)
+
